56140d59089794ae4b0bf14a79c26ece3aafc81e
brys broughton <brysbroughton24@gmail.com>
2020-04-30 23:11:42 -0400

    part c betweeness

--- networks_lib/betweenness.py
+++ networks_lib/betweenness.py
@@ -17,6 +17,59 @@ import numpy as np
 def edge_counts(vertex, mat):
     num_vertices = mat.shape[0]
     res = np.zeros((num_vertices, num_vertices))
+    
+    #list of all paths followed. path is a list of the nodes in order
+    paths = [[vertex]]
+    
+    #import pdb
+    #from pdb import set_trace as bp
+    #bp()
+    
+    def rec_edge_count(level_count, prev_level, cur_level):
+        if len(cur_level) == 0:
+            return
+        
+        for u in prev_level:
+            for v in cur_level:
+                if mat[u][v] == 1:
+                    for path in paths:
+                        if path[-1] == u:
+                            new_path = path + [v]
+                            paths.append(new_path)
+        for v in cur_level:
+            for path in paths:
+                if path[-1] == v:
+                    length = len(path)
+                    for i in range(0, length):
+                        if i == length - 1:
+                            break
+                        res[path[i]][path[i+1]] += 1
+                        res[path[i+1]][path[i]] += 1
+                    
+        #print(level_count, prev_level, cur_level, paths, res)
+        
+        current_neighbors = set([])
+        for node in cur_level:
+            current_neighbors = current_neighbors.union(np.where(mat[node] == 1)[0])
+        
+        #current_neighbors = list(set(current_neighbors))
+        
+        next_level = []
+        
+        for n in current_neighbors:
+            if n in prev_level:
+                continue
+            if n in cur_level:
+                continue
+            if n in next_level:
+                continue
+            next_level.append(n)
+            
+        rec_edge_count(level_count + 1, cur_level, next_level)
+    
+    level1 = np.where(mat[vertex] == 1)[0]
+    rec_edge_count(1, [vertex], level1)
+    
     return res
 
 ## Compute edge betweeness for a graph
